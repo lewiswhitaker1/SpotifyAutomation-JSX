790d2cc10641dfa8aa087aa8aac881ae753d13bf
lewiswhitaker1 <l.whitaker@activewindowfilms.co.uk>
2023-02-13 16:00:57 +0000

    still didn't work lol

--- Testing/show testing.jsx
+++ Testing/show testing.jsx
@@ -18,7 +18,7 @@
     trackJsonBat.open("w");
     trackJsonBat.write("set TRACKID=" + trackId + "\n");
     trackJsonBat.write("set AUTHTOKEN=" + authToken + "\n");
-    trackJsonBat.write('curl.exe -X "GET" "https://api.spotify.com/v1/show/%TRACKID%\" -H "Accept: application/json" -H "Content-Type: application/json" -H "Authorization: Bearer %AUTHTOKEN%" > %userprofile%\\Desktop\\track.json\n');
+    trackJsonBat.write('curl.exe -X "GET" "https://api.spotify.com/v1/shows/%TRACKID%\" -H "Accept: application/json" -H "Content-Type: application/json" -H "Authorization: Bearer %AUTHTOKEN%" > %userprofile%\\Desktop\\track.json\n');
     trackJsonBat.close();
 
     trackJsonBat.execute();
