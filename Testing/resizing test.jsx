6209fae0e6c678cac1a8f95f449acfe4e90329e3
lewiswhitaker1 <l.whitaker@activewindowfilms.co.uk>
2023-02-14 17:22:00 +0000

    Started testing to resize spotify code without depending on actions

---
+++ Testing/resizing test.jsx
@@ -0,0 +1,25 @@
+var doc = app.activeDocument;
+
+// Get the active layer
+var activeLayer = app.activeDocument.activeLayer;
+
+// Get the path items in the layer
+var pathItems = activeLayer.pathItems;
+
+// Check if there are any path items in the layer
+if (pathItems.length > 0) {
+    // Get the bottom path item
+    var bottomPath = pathItems[pathItems.length - 1];
+
+    // Remove the bottom path item
+    bottomPath.remove();
+}
+
+var selection = doc.selection;
+var group = doc.groupItems.add();
+for (var i = 0; i < selection.length; i++) {
+    selection[i].moveToBeginning(group);
+}
+
+group.width = 165.49;
+group.height = 78.185;
