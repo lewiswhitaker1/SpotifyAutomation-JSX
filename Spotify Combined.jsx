716ce384816bc20fedbc1446ccfd4c7333218e5e
lewiswhitaker1 <l.whitaker@activewindowfilms.co.uk>
2023-02-15 09:35:03 +0000

    No longer dependent on actions!

--- Spotify Combined.jsx
+++ Spotify Combined.jsx
@@ -687,3 +687,61 @@ function partThree() {
         f.close();
         f.execute();
     }
+
+    function resizeCode(widthInMm, heightInMm)
+    {
+      var doc = app.activeDocument;
+
+      // Get the active layer
+      var activeLayer = app.activeDocument.activeLayer;
+
+      // Get the path items in the layer
+      var pathItems = activeLayer.pathItems;
+
+      // Check if there are any path items in the layer
+      if (pathItems.length > 0) {
+          // Get the bottom path item
+          var bottomPath = pathItems[pathItems.length - 1];
+
+          // Remove the bottom path item
+          bottomPath.remove();
+      }
+
+      var myLayer = doc.layers.getByName("Layer 1");
+
+      var paths = [];
+      var compounds = [];
+
+      // Recursively find all paths and compound paths in the layer
+      function collectPaths(item) {
+          if (item.typename == "PathItem") {
+              paths.push(item);
+          } else if (item.typename == "CompoundPathItem") {
+              compounds.push(item);
+          } else if (item.typename == "GroupItem") {
+              for (var i = 0; i < item.pageItems.length; i++) {
+                  collectPaths(item.pageItems[i]);
+              }
+          }
+      }
+
+      // Get all paths and compound paths in the layer
+      for (var i = 0; i < myLayer.pageItems.length; i++) {
+          collectPaths(myLayer.pageItems[i]);
+      }
+
+      var group = myLayer.groupItems.add();
+      for (var i = 0; i < paths.length; i++) {
+          paths[i].moveToBeginning(group);
+      }
+      for (var i = 0; i < compounds.length; i++) {
+          compounds[i].moveToBeginning(group);
+      }
+
+      var widthInPt = widthInMm / 25.4 * 72;
+      var heightInPt = heightInMm / 25.4 * 72;
+
+      group.width = widthInPt;
+      group.height = heightInPt;
+
+    }
